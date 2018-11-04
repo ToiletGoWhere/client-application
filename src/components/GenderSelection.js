de57bf831737efee5f1b6d092a8e3ca89d31953f
Shi Tianyuan <tianyuan.shi@TYs-MacBook-Pro.local>
2018-11-04 19:02:00 +0800

    revert update

--- src/components/GenderSelection.js
+++ src/components/GenderSelection.js
@@ -1,70 +1,16 @@
 import React from "react";
 import { connect } from "dva";
-import { Accessible, Accessibility, PregnantWoman } from "@material-ui/icons";
-import styled from "styled-components";
-import SelectionButton from "./SelectionButton";
-
-const GenderSelectionContainer = styled.div`
-  height: 80px;
-  padding: 11px;
-  color: #4169e1;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-`;
-
+import styles from "./GenderSelection.css";
 class GenderSelection extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      activeGender: ""
-    };
+    this.state = {};
   }
   componentDidMount() {}
   componentWillUnmount() {}
 
   render() {
-    return (
-      <GenderSelectionContainer>
-        <div>Select Gender</div>
-        <SelectionButton
-          active={this.state.activeGender === "handicap"}
-          onClick={() => this.setState({ activeGender: "handicap" })}
-        >
-          <Accessible
-            style={
-              this.state.activeGender === "handicap"
-                ? { color: "#FFF" }
-                : { color: "#4169e1" }
-            }
-          />
-        </SelectionButton>
-        <SelectionButton
-          active={this.state.activeGender === "male"}
-          onClick={() => this.setState({ activeGender: "male" })}
-        >
-          <Accessibility
-            style={
-              this.state.activeGender === "male"
-                ? { color: "#FFF" }
-                : { color: "#4169e1" }
-            }
-          />
-        </SelectionButton>
-        <SelectionButton
-          active={this.state.activeGender === "female"}
-          onClick={() => this.setState({ activeGender: "female" })}
-        >
-          <PregnantWoman
-            style={
-              this.state.activeGender === "female"
-                ? { color: "#FFF" }
-                : { color: "#4169e1" }
-            }
-          />
-        </SelectionButton>
-      </GenderSelectionContainer>
-    );
+    return <div className={styles.base}>Hello World</div>;
   }
 }
 
--- src/components/ToiletOptions.js
+++ src/components/ToiletOptions.js
@@ -1,60 +1,6 @@
 import React from "react";
 import { connect } from "dva";
-// import styles from "./ToiletOptions.css";
-import styled from "styled-components";
-import GenderSelection from "./GenderSelection";
-
-const ToiletOptionsContainer = styled.div`
-  background-color: #f2f2f2;
-  width: 100%;
-  height: 300px;
-  left: 0;
-  bottom: 0;
-  position: fixed;
-  z-index: 999;
-`;
-
-const LocationErrorContainer = styled.div`
-  height: 40px;
-  color: #4169e1;
-  padding: 11px;
-  box-sizing: border-box;
-  cursor: pointer;
-`;
-const FloorSelectionContainer = styled.div`
-  height: 100px;
-  background-color: red;
-`;
-const ConfirmationContainer = styled.div`
-  height: 80px;
-  /* background-color: black; */
-`;
-
-const ConfirmationButton = styled.div`
-  height: 40px;
-  width: 353px;
-  border-radius: 5px;
-  padding-top: 8px;
-  left: 50%;
-  transform: translate(-50%, 50%);
-  cursor: pointer;
-  background: linear-gradient(
-    -45deg,
-    #4169e1,
-    #7363d6,
-    #925dc8,
-    #a858ba,
-    #b855ab,
-    #c3549c
-  );
-  text-align: center;
-  color: #fff;
-  font-size: 18px;
-  font-weight: 500;
-  box-sizing: border-box;
-  position: absolute;
-`;
-
+import styles from "./ToiletOptions.css";
 class ToiletOptions extends React.Component {
   constructor(props) {
     super(props);
@@ -64,19 +10,7 @@ class ToiletOptions extends React.Component {
   componentWillUnmount() {}
 
   render() {
-    return (
-      <ToiletOptionsContainer>
-        <LocationErrorContainer>
-          Not Correct Location? Relocate
-        </LocationErrorContainer>
-
-        <GenderSelection />
-        <FloorSelectionContainer>Floor Selection</FloorSelectionContainer>
-        <ConfirmationContainer>
-          <ConfirmationButton>CONFIRM</ConfirmationButton>
-        </ConfirmationContainer>
-      </ToiletOptionsContainer>
-    );
+    return <div className={styles.base}>Hello World</div>;
   }
 }
 
