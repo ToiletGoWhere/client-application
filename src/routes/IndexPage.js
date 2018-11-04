d2199923e029f1a8b13c5f6862fa8a34a16a923b
Shi Tianyuan <tianyuan.shi@TYs-MacBook-Pro.local>
2018-11-04 19:03:23 +0800

    revert index page

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { connect } from "dva";
 import styles from "./IndexPage.css";
-// import Example from "../components/Example";
-import ToiletOptions from "../components/ToiletOptions";
+import Example from "../components/Example";
 function IndexPage() {
   return (
     <div className={styles.normal}>
-      <ToiletOptions />
+      <Example />
     </div>
   );
 }
