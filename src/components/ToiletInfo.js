f0edd92cefef925baa5559863fa9b1fc46a7a29f
Doriarry <mac@DNdeMacBook-Pro.local>
2018-11-10 13:11:24 +0800

    toilet info

--- src/components/ToiletInfo.js
+++ src/components/ToiletInfo.js
@@ -10,135 +10,158 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
+import FemaleIcon from "../assets/icons/FemaleIcon";
+import MaleIcon from "../assets/icons/MaleIcon";
+import NursingIcon from "../assets/icons/NursingIcon";
+import axios from "axios";
 
 const RatingContainer = styled.div`
-  width: 100%;
-  text-align: center;
+    width: 100%;
+    text-align: center;
 `;
 
 const cardStyles = theme => ({
-  root: {
-    width: "100%",
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-    position: "relative",
-    overflow: "auto",
-    maxHeight: 300
-  },
-  listSection: {
-    backgroundColor: "inherit"
-  },
-  ul: {
-    backgroundColor: "inherit",
-    padding: 0
-  }
+    root: {
+        width: "100%",
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+        position: "relative",
+        overflow: "auto",
+        maxHeight: 300,
+    },
+    listSection: {
+        backgroundColor: "inherit",
+    },
+    ul: {
+        backgroundColor: "inherit",
+        padding: 0,
+    },
 });
 
 class ToiletInfo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-  componentDidMount() {}
-  componentWillUnmount() {
-    this.props.dispatch(loadData(this.props.userId));
-  }
+    componentDidMount() {}
 
-  render() {
-    //this.captureData();
+    loadData(id) {
+        axios.get(`172.31.38.253:3000/api/feedbacks/${id}`).then(res => {
+            return res.data;
+        });
+    }
 
-    return (
-      <div className={styles.General}>
-        <div className={styles.gender__container}>
-          <div className={styles.Display}>Gender:</div>
-          {this.props.toiletData.gender === "handicap" && (
-            <SelectionButton
-              active
-              onChange={() =>
-                this.props.dispatch({
-                  type: "toiletData/save",
-                  payload: {
-                    gender: "handicap"
-                  }
-                })
-              }
-            >
-              <Accessible style={{ color: "#FFF" }} />
-            </SelectionButton>
-          )}
+    componentWillUnmount() {}
 
-          {this.props.toiletData.gender === "male" && (
-            <SelectionButton
-              active
-              onChange={() =>
-                this.props.dispatch({
-                  type: "toiletData/save",
-                  payload: {
-                    gender: "male"
-                  }
-                })
-              }
-            >
-              <Accessibility style={{ color: "#FFF" }} />
-            </SelectionButton>
-          )}
+    render() {
+        return (
+            <div className={styles.General}>
+                <div className={styles.gender__container}>
+                    <div className={styles.Display}>Gender:</div>
+                    {this.props.toiletData.gender === "accessible" && (
+                        <SelectionButton
+                            active
+                            onChange={() =>
+                                this.props.dispatch({
+                                    type: "toiletData/save",
+                                    payload: {
+                                        gender: "accessible",
+                                    },
+                                })
+                            }
+                        >
+                            <Accessible style={{ color: "#FFF" }} />
+                        </SelectionButton>
+                    )}
 
-          {this.props.toiletData.gender === "female" && (
-            <SelectionButton
-              active
-              onChange={() =>
-                this.props.dispatch({
-                  type: "toiletData/save",
-                  payload: {
-                    gender: "female"
-                  }
-                })
-              }
-            >
-              <PregnantWoman style={{ color: "#FFF" }} />
-            </SelectionButton>
-          )}
-        </div>
+                    {this.props.toiletData.gender === "male" && (
+                        <SelectionButton
+                            active
+                            onChange={() =>
+                                this.props.dispatch({
+                                    type: "toiletData/save",
+                                    payload: {
+                                        gender: "male",
+                                    },
+                                })
+                            }
+                        >
+                            <MaleIcon style={{ color: "#FFF" }} />
+                        </SelectionButton>
+                    )}
 
-        <div className={styles.DisplayOut}>
-          Rating:
-          <Rate
-            className={styles.DisplayRat}
-            disabled
-            allowHalf
-            value={this.props.currentToiletSelected.rating}
-          />
-        </div>
+                    {this.props.toiletData.gender === "female" && (
+                        <SelectionButton
+                            active
+                            onChange={() =>
+                                this.props.dispatch({
+                                    type: "toiletData/save",
+                                    payload: {
+                                        gender: "female",
+                                    },
+                                })
+                            }
+                        >
+                            <FemaleIcon style={{ color: "#FFF" }} />
+                        </SelectionButton>
+                    )}
 
-        <div className={styles.Display}>Review:</div>
-        <div className={styles.Root}>
-          {this.fetch(
-            "api/feedbacks/{this.props.currentToiletSelected._Id}}"
-          ).map((item, i) => {
-            return (
-              <List>
-                <li>
-                  <ul className={styles.Ul}>
-                    <ListItem key={i}>
-                      <ListItemText primary={item} />
-                    </ListItem>
-                  </ul>
-                </li>
-                <Divider className={styles.Divi} />
-              </List>
-            );
-          })}
-        </div>
-      </div>
-    );
-  }
+                    {this.props.toiletData.gender === "nursing" && (
+                        <SelectionButton
+                            active
+                            onChange={() =>
+                                this.props.dispatch({
+                                    type: "toiletData/save",
+                                    payload: {
+                                        gender: "nursing",
+                                    },
+                                })
+                            }
+                        >
+                            <NursingIcon style={{ color: "#FFF" }} />
+                        </SelectionButton>
+                    )}
+                </div>
+
+                <div className={styles.DisplayOut}>
+                    Rating:
+                    <Rate
+                        className={styles.DisplayRat}
+                        disabled
+                        allowHalf
+                        value={this.props.currentToiletSelected.rating}
+                    />
+                </div>
+
+                <div className={styles.Display}>Review:</div>
+                <div className={styles.Root}>
+                    {this.loadData(this.props.currentToiletSelected._id).map(
+                        (item, i) => {
+                            return (
+                                <List>
+                                    <li>
+                                        <ul className={styles.Ul}>
+                                            <ListItem key={i}>
+                                                <ListItemText primary={item} />
+                                            </ListItem>
+                                        </ul>
+                                    </li>
+                                    <Divider className={styles.Divi} />
+                                </List>
+                            );
+                        },
+                    )}
+                </div>
+            </div>
+        );
+    }
 }
 
 ToiletInfo.propTypes = {};
 
 function mapStateToProps(state) {
-  return state;
+    return state;
 }
 
 export default connect(mapStateToProps)(ToiletInfo);
