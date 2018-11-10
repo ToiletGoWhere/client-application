46895709420da8c222cc69637b30c9d55ff7af96
Shi Tianyuan <tianyuan.shi@TYs-MacBook-Pro.local>
2018-11-10 15:44:24 +0800

    update login page

--- src/routes/LoginPage.js
+++ src/routes/LoginPage.js
@@ -2,12 +2,25 @@ import React from "react";
 import { connect } from "dva";
 import styles from "./LoginPage.css";
 import ToiletOptions from "../components/ToiletOptions";
-function LoginPage() {
-    return (
-        <div className={styles.normal}>
-            <ToiletOptions />
-        </div>
-    );
+import { withStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import TextField from "@material-ui/core/TextField";
+class LoginPage extends React.Component() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+        };
+    }
+    render() {
+        return (
+            <div className={styles.normal}>
+                <div />
+                Login Page
+            </div>
+        );
+    }
 }
 
 LoginPage.propTypes = {};
