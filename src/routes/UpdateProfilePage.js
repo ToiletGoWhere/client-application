06ab727975862c570262c0a670ae75852a331f87
Shi Tianyuan <tianyuan.shi@r-75-101-25-172.comp.nus.edu.sg>
2018-11-13 20:21:51 +0800

    Fix Update alert

--- src/routes/UpdateProfilePage.js
+++ src/routes/UpdateProfilePage.js
@@ -3,9 +3,11 @@ import { connect } from "dva";
 import styles from "./IndexPage.css";
 import UpdateProfile from "../components/UpdateProfile";
 import BackButton from "../components/BackButton";
+import InformationBar from "../components/InformationBar";
 function UpdateProfilePage() {
     return (
         <div className={styles.normal}>
+            <InformationBar />
             <BackButton type="profile" />
             <UpdateProfile />
         </div>
